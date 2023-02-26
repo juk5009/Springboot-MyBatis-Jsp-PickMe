e85f8784afa72038e2d41e1dafc91877e2fc565a
Kjuk <juk6340@gmail.com>
2023-02-26 14:46:39 +0900

    project 환경 세팅 완료

---
+++ src/main/java/shop/mtcoding/pickme/PickmeApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.pickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/pickme/PickmeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.pickme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
