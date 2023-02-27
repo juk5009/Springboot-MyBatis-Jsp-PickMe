0e295582e4ac5cf83e8781f712b6d96f07ec05d0
wonbin3366 <wonbin3366@naver.com>
2023-02-27 12:24:22 +0900

    띄어쓰기 수정 완료

--- src/main/java/shop/mtcoding/pickme/controller/ResumeController.java
+++ src/main/java/shop/mtcoding/pickme/controller/ResumeController.java
@@ -1,19 +1,7 @@
 package shop.mtcoding.pickme.controller;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import shop.mtcoding.pickme.dto.user.UserReq.UserJoinReqDto;
-import shop.mtcoding.pickme.dto.user.UserReq.UserLoginReqDto;
-import shop.mtcoding.pickme.handler.ex.CustomException;
-import shop.mtcoding.pickme.model.User;
-import shop.mtcoding.pickme.model.UserRepository;
-import shop.mtcoding.pickme.service.UserService;
 
 @Controller
 public class ResumeController {
