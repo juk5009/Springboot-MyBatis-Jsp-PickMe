53d914549a0892d3a92f34d97dc6879e1a9aa817
khh5762 <khh5762@naver.com>
2023-02-27 18:48:26 +0900

    Resume Repository, Controller 테스트 완료

---
+++ src/test/java/shop/mtcoding/pickme/model/ResumeRepositoryTest.java
@@ -0,0 +1,50 @@
+package shop.mtcoding.pickme.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.pickme.dto.resume.ResumeReqDto.ResumeSaveReqDto;
+
+@MybatisTest
+public class ResumeRepositoryTest {
+
+    @Autowired
+    private ResumeRepository resumeRepository;
+
+    @Test
+    public void insert_test() throws Exception {
+
+        // given
+        ResumeSaveReqDto resumeSaveReqDto = new ResumeSaveReqDto();
+        resumeSaveReqDto.setUserId(1);
+        resumeSaveReqDto.setResumeName("홍길동");
+        resumeSaveReqDto.setResumeBirth("1997-02-10");
+        resumeSaveReqDto.setResumeEmail("hong@nate.com");
+        resumeSaveReqDto.setResumeAddress("부산 사상");
+        resumeSaveReqDto.setResumeLocation("부산");
+        resumeSaveReqDto.setResumeGrade("전문학사");
+        resumeSaveReqDto.setResumePhoneNumber("01055558888");
+        resumeSaveReqDto.setResumeSex("남");
+        resumeSaveReqDto.setResumeContent("너너넝");
+
+
+        System.out.println("테스트2 pw : " + resumeSaveReqDto.getResumePhoneNumber());
+
+        ObjectMapper om = new ObjectMapper();
+        // when
+        int reuslt = resumeRepository.insert(resumeSaveReqDto);
+        String responseBody = om.writeValueAsString(reuslt); 
+        System.out.println("테스트 : " + responseBody);
+       
+
+        // then
+        assertThat(reuslt).isEqualTo(1);
+
+    }
+
+}
