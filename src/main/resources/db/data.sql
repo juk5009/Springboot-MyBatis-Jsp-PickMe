69563ee2467d7e289446df7c5259e32ff0ce1a7a
Kjuk <juk6340@gmail.com>
2023-02-27 10:55:12 +0900

    db 및 더미데이터 수정 완료

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,22 +1,22 @@
-INSERT INTO user_tb(user_id, user_password, user_name, user_birth, user_email, user_address, user_location, user_grade, user_phone_number, user_sex, user_profile, created_at) values('ssar', '1234', '쌀', '19910329', 'ssar@nate.com', '부산광역시 북구 덕천동', '부산', '고졸', '01011112222', '남', '/images/dora.png', now());
-INSERT INTO user_tb(user_id, user_password, user_name, user_birth, user_email, user_address, user_location, user_grade, user_phone_number, user_sex, user_profile, created_at) values('bori', '1234', '보리', '19950728', 'bori@nate.com', '부산광역시 연제구 연산동', '서울', '대졸', '01022223333', '여', '/images/dora.png', now());
-INSERT INTO user_tb(user_id, user_password, user_name, user_birth, user_email, user_address, user_location, user_grade, user_phone_number, user_sex, user_profile, created_at) values('juho', '1234', '주호', '19860528', 'joho@gmail.com', '부산광역시 해운대구 반송동', '대구', '초대졸', '01033334444', '남', '/images/dog.jpg', now());
+INSERT INTO user_tb(user_name, user_password, user_email, created_at) values('ssar', '1234', 'ssar@nate.com', now());
+INSERT INTO user_tb(user_name, user_password, user_email, created_at) values('bori', '1234', 'bori@nate.com', now());
+INSERT INTO user_tb(user_name, user_password, user_email, created_at) values('juho', '1234', 'joho@gmail.com', now());
 
-INSERT INTO company_tb(company_id, company_password, company_name, company_email, company_address, company_location, company_tel, company_profile, created_at) values('samsung', '1234', '삼성', 'samsung@gmail.com', '부산광역시 북구 덕천동', '부산', '05111112222', '/images/samsung.png', now());
-INSERT INTO company_tb(company_id, company_password, company_name, company_email, company_address, company_location, company_tel, company_profile, created_at) values('lg', '1234', '엘지', 'lg@gmail.com', '서울특별시 동작구 대림동', '서울', '0222223333', '/images/lg.png', now());
-INSERT INTO company_tb(company_id, company_password, company_name, company_email, company_address, company_location, company_tel, company_profile, created_at) values('green', '1234', '그린', 'green@gmail.com', '부산광역시 해운대구 반송동', '부산', '05133334444', '/images/samsung.png', now());
+INSERT INTO company_tb(company_name, company_password, company_email, created_at) values('samsung', '1234', 'samsung@gmail.com', now());
+INSERT INTO company_tb(company_name, company_password, company_email, created_at) values('lg', '1234', 'lg@gmail.com', now());
+INSERT INTO company_tb(company_name, company_password, company_email, created_at) values('green', '1234', 'green@gmail.com', now());
 
-INSERT INTO companyinfo_tb(builddate, salemoney, owner_name, companyinfo_adress, employees, created_at) values('2003년 2월 23일', '178억', '김민수', '부산광역시 북구 덕천동', '530명', now());
-INSERT INTO companyinfo_tb(builddate, salemoney, owner_name, companyinfo_adress, employees, created_at) values('1997년 7월 25일', '802억', '임민수', '부산광역시 연제구 연산동', '730명', now());
-INSERT INTO companyinfo_tb(builddate, salemoney, owner_name, companyinfo_adress, employees, created_at) values('2001년 5월 15일', '1574억', '임원빈', '부산광역시 해운대구 반송동', '1213명', now());
+INSERT INTO companyinfo_tb(company_id, builddate, salemoney, owner_name, companyinfo_adress, employees, created_at) values('1', '2003년 2월 23일', '178억', '김민수', '부산광역시 북구 덕천동', '530명', now());
+INSERT INTO companyinfo_tb(company_id, builddate, salemoney, owner_name, companyinfo_adress, employees, created_at) values('1', '1997년 7월 25일', '802억', '임민수', '부산광역시 연제구 연산동', '730명', now());
+INSERT INTO companyinfo_tb(company_id, builddate, salemoney, owner_name, companyinfo_adress, employees, created_at) values('2', '2001년 5월 15일', '1574억', '임원빈', '부산광역시 해운대구 반송동', '1213명', now());
 
-INSERT INTO notice_tb(notice_title, notice_employtype, notice_content, notice_location, notice_career, notice_grade, notice_pay, company_mark, created_at) values('자바 기술자 모집합니다.', '정규직', '자바 기술자를 뽑지만 스프링도 할줄 알아야 하고, 리액트도 할줄 아셔야 합니다.', '부산', '경력 2년이상', '초대졸이상', '5000', '/images/samsung.png', now());
-INSERT INTO notice_tb(notice_title, notice_employtype, notice_content, notice_location, notice_career, notice_grade, notice_pay, company_mark, created_at) values('스프링 기술자 모집합니다.', '정규직', '스프링 기술자를 뽑지만 리액트도 할줄 알아야 하고, 플러터도 할줄 아셔야 합니다.', '서울', '경력 5년이상', '대졸이상', '1000', '/images/lg.png', now());
-INSERT INTO notice_tb(notice_title, notice_employtype, notice_content, notice_location, notice_career, notice_grade, notice_pay, company_mark, created_at) values('플러터 기술자 모집합니다.', '계약직', '플러터 기술자를 뽑지만 스프링도 할줄 알아야 하고, 자바도 할줄 아셔야 합니다.', '부산', '신입' , '고졸이상', '3000', '/images/samsung.png', now());
+INSERT INTO notice_tb(company_id, notice_title, notice_employtype, notice_content, notice_location, notice_career, notice_grade, notice_pay, company_mark, created_at) values('1', '자바 기술자 모집합니다.', '정규직', '자바 기술자를 뽑지만 스프링도 할줄 알아야 하고, 리액트도 할줄 아셔야 합니다.', '부산', '경력 2년이상', '초대졸이상', '5000', '/images/samsung.png', now());
+INSERT INTO notice_tb(company_id, notice_title, notice_employtype, notice_content, notice_location, notice_career, notice_grade, notice_pay, company_mark, created_at) values('1', '스프링 기술자 모집합니다.', '정규직', '스프링 기술자를 뽑지만 리액트도 할줄 알아야 하고, 플러터도 할줄 아셔야 합니다.', '서울', '경력 5년이상', '대졸이상', '1000', '/images/lg.png', now());
+INSERT INTO notice_tb(company_id, notice_title, notice_employtype, notice_content, notice_location, notice_career, notice_grade, notice_pay, company_mark, created_at) values('2', '플러터 기술자 모집합니다.', '계약직', '플러터 기술자를 뽑지만 스프링도 할줄 알아야 하고, 자바도 할줄 아셔야 합니다.', '부산', '신입' , '고졸이상', '3000', '/images/samsung.png', now());
 
-INSERT INTO apply_tb(user_id, notice_id, created_at) values('ssar', '1', now());
-INSERT INTO apply_tb(user_id, notice_id, created_at) values('bori', '2', now());
-INSERT INTO apply_tb(user_id, notice_id, created_at) values('juho', '1', now());
+INSERT INTO apply_tb(resume_id, notice_id, created_at) values('1', '1', now());
+INSERT INTO apply_tb(resume_id, notice_id, created_at) values('1', '2', now());
+INSERT INTO apply_tb(resume_id, notice_id, created_at) values('1', '1', now());
 
 INSERT INTO skill_tb(skill_java, skill_java_script, skill_spring, skill_jsp, skill_react, skill_flutter, created_at) values('java', null, null, null, null, null, now());
 INSERT INTO skill_tb(skill_java, skill_java_script, skill_spring, skill_jsp, skill_react, skill_flutter, created_at) values(null, 'java script', null, null, null, null, now());
--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -1,44 +1,33 @@
 CREATE TABLE user_tb( 
     id int auto_increment primary key,
-	user_id varchar not null,
-	user_password varchar not null,
-	user_name varchar,
-	user_birth varchar,
-	user_email varchar not null,
-	user_address varchar(100), 
-	user_location varchar,
-	user_grade varchar,
-	user_phone_number varchar,
-	user_sex varchar,
-	user_profile varchar,
+	user_name varchar not null,
+	user_password varchar not null,	
+	user_email varchar not null,	
     created_at timestamp not null
 );
 
 CREATE TABLE company_tb( 
     id int auto_increment primary key,
-	company_id varchar not null,
-	company_password varchar not null,
-	company_name varchar,
-	company_email varchar not null,	
-	company_address varchar,
-	company_location varchar,
-	company_tel varchar,
-	company_profile varchar,
+	company_name varchar not null,
+	company_password varchar not null,	
+	company_email varchar not null,		
 	created_at timestamp not null
 );
 
 CREATE TABLE companyinfo_tb(
     id int auto_increment primary key,
-    builddate varchar not null,
-    salemoney varchar not null,
-    owner_name varchar not null,
-    companyinfo_adress varchar not null,
-    employees varchar not null,
+	company_id int, 
+    builddate varchar,
+    salemoney varchar,
+    owner_name varchar,
+    companyinfo_adress varchar,
+    employees varchar,
     created_at timestamp not null
 );
 
 CREATE TABLE notice_tb( 
     id int auto_increment primary key,
+	company_id int,
     notice_title varchar not null,
 	notice_employtype varchar not null,
 	notice_content varchar not null,
@@ -59,11 +48,12 @@ CREATE TABLE apply_tb(
 
 CREATE TABLE skill_tb(
     id int auto_increment primary key,
-    skill_java varchar,
-    skill_java_script varchar,
-    skill_spring varchar,
-    skill_jsp varchar,
-    skill_react varchar,
-    skill_flutter varchar,
+	resume_id int not null,
+    skill_name1 varchar,
+    skill_name2 varchar,
+    skill_name3 varchar,
+    skill_name4 varchar,
+    skill_name5 varchar,
+    skill_name6 varchar,
     created_at timestamp not null
 );
