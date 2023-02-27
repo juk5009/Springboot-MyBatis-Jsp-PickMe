c1da433a0f3f72f61c80efda9f0c79b96ae09505
khh5762 <khh5762@naver.com>
2023-02-27 18:45:25 +0900

    resume, userskill mapper, Repository 작성 완료

---
+++ src/main/java/shop/mtcoding/pickme/model/ResumeRepository.java
@@ -0,0 +1,28 @@
+package shop.mtcoding.pickme.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import shop.mtcoding.pickme.dto.resume.ResumeReqDto.ResumeSaveReqDto;
+
+@Mapper
+public interface ResumeRepository {
+
+        public ResumeSaveReqDto findByUserIdWithResume(int id);
+
+        public List<Resume> findAll();
+
+        public User findById(int id);
+
+        public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
+        public User findByUsername(@Param("username") String username);
+
+        public int insert(ResumeSaveReqDto resumeSaveReqDto);
+
+        public int updateById(ResumeSaveReqDto resumeSaveReqDto);
+
+        public int deleteById(int id);
+}
