b49e36a8b002f534917a393c332bbc4eaeface89
khh5762 <khh5762@naver.com>
2023-02-27 18:46:01 +0900

    resume, userskill DTO 작성 완료

---
+++ src/main/java/shop/mtcoding/pickme/dto/resume/ResumeReqDto.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.pickme.dto.resume;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+public class ResumeReqDto {
+
+    @Getter
+    @Setter
+    public static class ResumeSaveReqDto {
+        private Integer userId;
+        private String resumeName;
+        private String resumeBirth;
+        private String resumeEmail;
+        private String resumeAddress;
+        private String resumeLocation;
+        private String resumeGrade;
+        private String resumePhoneNumber;
+        private String resumeSex;
+        private String resumeContent;
+    }
+}
