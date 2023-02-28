75841c575315c1bbb63119fa6a997b6bb49f0f47
Kjuk <juk6340@gmail.com>
2023-02-28 15:58:04 +0900

    NoticeDto 생성완료

---
+++ src/main/java/shop/mtcoding/pickme/dto/notice/NoticeReq.java
@@ -0,0 +1,20 @@
+package shop.mtcoding.pickme.dto.notice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class NoticeReq {
+    @Getter
+    @Setter
+    public static class SaveNoticeReqDto {
+        private Integer companyId;
+        private String noticeTitle;
+        private String noticeCareer;
+        private String noticePay;
+        private String noticeEmploytype;
+        private String noticeGrade;
+        private String noticeLocation;
+        private String noticeContent;
+    }
+
+}
