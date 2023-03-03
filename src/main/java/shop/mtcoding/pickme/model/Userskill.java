ffe78069f1fdb0b197802ade0f26fb3e1676a0e5
aj124578 <aj124578@naver.com>
2023-03-03 11:22:34 +0900

    Userskill mapper 수정, Resume, Userskill 모델, UserskillRepository, ResumeRepository 수정 및 추가 완료

--- src/main/java/shop/mtcoding/pickme/model/Userskill.java
+++ src/main/java/shop/mtcoding/pickme/model/Userskill.java
@@ -2,15 +2,24 @@ package shop.mtcoding.pickme.model;
 
 import java.sql.Timestamp;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import shop.mtcoding.pickme.dto.resume.ResumeReq.ResumeSaveReqDto;
 
-@AllArgsConstructor
+
 @Getter
 @Setter
 public class Userskill {
+    private Integer id;
     private Integer resumeId;
+    private Integer userId;
     private String userskillName;
     private Timestamp createdAt;
+
+    public Userskill(ResumeSaveReqDto resumeSaveReqDto) {
+        this.userskillName = resumeSaveReqDto.getUserskillList();
+        this.resumeId = resumeSaveReqDto.getId();
+        this.userId = resumeSaveReqDto.getUserId();
+    }
+
 }
--- src/main/java/shop/mtcoding/pickme/model/UserskillRespository.java
+++ src/main/java/shop/mtcoding/pickme/model/UserskillRespository.java
@@ -3,6 +3,7 @@ package shop.mtcoding.pickme.model;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import shop.mtcoding.pickme.dto.userskill.UserskillReqDto.UserskillSaveReqDto;
 
@@ -12,7 +13,7 @@ public interface UserskillRespository {
 
         public User findById(int id);
 
-        public int insert(UserskillSaveReqDto userskillSaveReqDto);
+        public int insert(@Param("resumeId") int resumeId, @Param("userId") int userId, @Param("userskillName") String userskillName);
 
         public int updateById(UserskillSaveReqDto userskillSaveReqDto);
 
