9bdee7f30f98057c58cc7b7757dbced30f2ed2a8
Kjuk <juk6340@gmail.com>
2023-03-05 12:52:58 +0900

    modeling 및 mapper 완료

--- src/main/java/shop/mtcoding/pickme/dto/notice/NoticeReq.java
+++ src/main/java/shop/mtcoding/pickme/dto/notice/NoticeReq.java
@@ -20,4 +20,20 @@ public class NoticeReq {
         private String noticeContent;
         private String companyskillList;
     }
+
+    @Getter
+    @Setter
+    public static class NoticeUpdateReqDto {
+        private Integer id;
+        private Integer companyId;
+        private String noticeCompanyname;
+        private String noticeTitle;
+        private String noticeCareer;
+        private String noticePay;
+        private String noticeEmploytype;
+        private String noticeGrade;
+        private String noticeLocation;
+        private String noticeContent;
+        private String companyskillList;
+    }
 }
