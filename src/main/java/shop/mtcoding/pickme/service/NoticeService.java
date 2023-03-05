22087d6f2fb62854ad87d73d596a88cb52911738
Kjuk <juk6340@gmail.com>
2023-03-05 11:28:05 +0900

    공고삭제 Service 완료

--- src/main/java/shop/mtcoding/pickme/service/NoticeService.java
+++ src/main/java/shop/mtcoding/pickme/service/NoticeService.java
@@ -27,7 +27,7 @@ public class NoticeService {
 
     @Transactional
     public void 공고작성(NoticeSaveReqDto noticeSaveReqDto, int comPrincipalId, String comSkill) {
-         /* 세션에 있는 id값을 set 해줌 */
+        /* 세션에 있는 id값을 set 해줌 */
         noticeSaveReqDto.setCompanyId(comPrincipalId);
 
         Notice notice = new Notice(noticeSaveReqDto);
@@ -37,37 +37,47 @@ public class NoticeService {
         System.out.println("테스트31 notice grade : " + notice.getNoticeGrade());
         System.out.println("테스트31 notice Id : " + notice.getId()); // null
 
-
         int result = noticeRepository.insert(notice);
         if (result != 1) {
             throw new CustomApiException("공고작성실패", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-         // insert 성공 후 notice 증가한 id(pk)값을 set 해줌 
-         noticeSaveReqDto.setId(notice.getId()); 
-         // insert 성공 후 notice companyId값을 set 해줌 
-         noticeSaveReqDto.setCompanyId(notice.getCompanyId());  
- 
-         /* checkbox의 체크된 값을 string으로 받아 왔기 때문에 split 하여 list에 담아줌 */
-         List<String> companyskillList = Arrays.asList(comSkill.split(","));
-         Companyskill com = new Companyskill(noticeSaveReqDto);  
-         System.out.println("테스트 Companyskill : " + com.getCompanyskillName());
-         System.out.println("테스트44 : " + companyskillList.size());
-         System.out.println("테스트444 : " + com.getNoticeId());
-         System.out.println("테스트445 : " + com.getCompanyId());
-         
- 
-         /* list 내용을 for문 돌려서 companyskill_tb에 insert 해줌 */
-         if (companyskillList != null) {
-             for (String companyskill : companyskillList) {
- 
-                 int result2 = companyskillRepository.insert(com.getNoticeId(), com.getCompanyId(), companyskill);
-                 if (result2 != 1) {
-                     throw new CustomApiException("요구 기술작성 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+        // insert 성공 후 notice 증가한 id(pk)값을 set 해줌
+        noticeSaveReqDto.setId(notice.getId());
+        // insert 성공 후 notice companyId값을 set 해줌
+        noticeSaveReqDto.setCompanyId(notice.getCompanyId());
 
-                 }
-                 System.out.println("테스트55 : " + companyskill);
-             }
- 
-         }
+        /* checkbox의 체크된 값을 string으로 받아 왔기 때문에 split 하여 list에 담아줌 */
+        List<String> companyskillList = Arrays.asList(comSkill.split(","));
+        Companyskill com = new Companyskill(noticeSaveReqDto);
+        System.out.println("테스트 Companyskill : " + com.getCompanyskillName());
+        System.out.println("테스트44 : " + companyskillList.size());
+        System.out.println("테스트444 : " + com.getNoticeId());
+        System.out.println("테스트445 : " + com.getCompanyId());
+
+        /* list 내용을 for문 돌려서 companyskill_tb에 insert 해줌 */
+        if (companyskillList != null) {
+            for (String companyskill : companyskillList) {
+
+                int result2 = companyskillRepository.insert(com.getNoticeId(), com.getCompanyId(), companyskill);
+                if (result2 != 1) {
+                    throw new CustomApiException("요구 기술작성 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+
+                }
+                System.out.println("테스트55 : " + companyskill);
+            }
+
+        }
+    }
+
+    public void 공고삭제(int id, int companyId) {
+        Notice noticePS = noticeRepository.findById(id);
+        if (noticePS == null) {
+            throw new CustomApiException("없는 공고입니다.");
+        }
+        if (noticePS.getCompanyId() != companyId) {
+            throw new CustomApiException("해당 공고를 삭제할 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+        noticeRepository.deleteById(id);
+
     }
 }
